9f35e94dd2b34884d488f3e143f343d7aa275d27
Lucas Dias Leandro <147958304+lucasserdev@users.noreply.github.com>
2024-07-01 17:29:39 -0300

    add speed
    a cada quantidade x de pontuação a velocidade é aumentada

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,8 @@ const gameOver = document.querySelector('.gameOver');
 const restart = document.querySelector('.restart');
 
 let gameOn = false;
+let score = 0;
+
 
 const jump = () => {
     mario.classList.add('jump');
@@ -19,6 +21,17 @@ const loop = setInterval(() => {
     const pipePosition = pipe.offsetLeft;
     const cloudsPosition = clouds.offsetLeft;
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+    if (score > 0 && score <= 60) {
+        pipe.style.animation = 'pipe-animation 3s linear infinite'; 
+    } else if (score > 60 && score <= 100) {
+        pipe.style.animation = 'pipe-animation 2s linear infinite';
+        
+    } else if (score > 100 && score <= 200) {
+        pipe.style.animation = 'pipe-animation 1s linear infinite';
+    } else {
+        pipe.style.animation = 'pipe-animation .5s linear infinite';
+    }
     
     
     if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
@@ -45,20 +58,21 @@ const loop = setInterval(() => {
 
 }, 10);
 
-let score = 0;
-
 const points = setInterval(() => {
     
     score++;
 
+   
+
     const scores = document.querySelector('.score');
     scores.textContent = score;
+
 
     if (gameOn === true) {
         clearInterval(points);
     }
 
-}, 200);
+}, 100);
 
 restart.addEventListener('click', () => {
     location.reload();
